af3662ec920802755e01fa4a3cd97e3c61fd31b6
alexeykostenko <aleksey.kostenko@edipresse.ua>
2017-08-17 18:11:15 +0300

    changed file resources/assets/js/voyager_tinymce.js

--- resources/assets/js/voyager_tinymce.js
+++ resources/assets/js/voyager_tinymce.js
@@ -1,7 +1,3 @@
-function setImageValue(url){
-  $('.mce-btn.mce-open').parent().find('.mce-textbox').val(url);
-}
-
 $(document).ready(function(){
 
   $.ajaxSetup({
@@ -10,23 +6,40 @@ $(document).ready(function(){
       }
   });
 
-  tinymce.init({
-    menubar: false,
-    selector:'textarea.richTextBox',
-    skin: 'voyager',
-    min_height: 600,
-    resize: 'vertical',
-    plugins: 'link, image, code, youtube, giphy, table, textcolor',
-    extended_valid_elements : 'input[id|name|value|type|class|style|required|placeholder|autocomplete|onclick]',
-    file_browser_callback: function(field_name, url, type, win) {
-            if(type =='image'){
-              $('#upload_file').trigger('click');
-            }
-        },
-    toolbar: 'styleselect bold italic underline | forecolor backcolor | alignleft aligncenter alignright | bullist numlist outdent indent | link image table youtube giphy | code',
-    convert_urls: false,
-    image_caption: true,
-    image_title: true
-  });
+  var editor_config = {
+    path_absolute : "/",
+    selector: "textarea.richTextBox",
+    plugins: [
+      "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+      "searchreplace wordcount visualblocks visualchars code fullscreen",
+      "insertdatetime media nonbreaking save table contextmenu directionality",
+      "emoticons template paste textcolor colorpicker textpattern"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+    relative_urls: false,
+    file_browser_callback : function(field_name, url, type, win) {
+        console.log(field_name, url, type, win);
+      var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+      var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+      var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+      if (type == 'image') {
+        cmsURL = cmsURL + "&type=Images";
+      } else {
+        cmsURL = cmsURL + "&type=Files";
+      }
+
+      tinyMCE.activeEditor.windowManager.open({
+        file : cmsURL,
+        title : 'Filemanager',
+        width : x * 0.8,
+        height : y * 0.8,
+        resizable : "yes",
+        close_previous : "no"
+      });
+    }
+  };
+
+  tinymce.init(editor_config);
 
 });
